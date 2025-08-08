6e9970aed21aac6a91718456fc3570eed2204731
Stefano Amenta <git.stefano@outlook.it>
2025-08-08 21:01:29 +0200

    sposrtato componenti prodotti

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
-import { ProductsComponent } from './components/products/products.component';
+import { ProductsComponent } from './components/products/show/products.component';
 import { LoginComponent } from './components/login/login.component';
 import { ForgotComponent } from './components/forgot/forgot/forgot.component';
 import { RegisterComponent } from './components/register/register/register.component';
+import { AddProductComponent } from './components/products/add/add-product/add-product.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'forgot', component: ForgotComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'products', component: ProductsComponent }
+  { path: 'products', component: ProductsComponent },
+  { path: 'products/add', component: AddProductComponent },
 ];
+
+//AddProductComponent
